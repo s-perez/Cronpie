b4a3f5e0a425a72fc76828e293ef4841a93959ac
Salvador Pérez <sisekeom@gmail.com>
2016-11-19 14:19:02 +0000

    Add timestamp to Run model

--- cronpie/models.py
+++ cronpie/models.py
@@ -1,6 +1,14 @@
-from peewee import Model, CharField, IntegerField, DateTimeField
+from peewee import (
+    CharField,
+    DateTimeField,
+    IntegerField,
+    ForeignKeyField,
+    Model,
+    SqliteDatabase
+)
 
 database = SqliteDatabase('measures.db')
+
 
 class BaseModel(Model):
     class Meta:
@@ -11,13 +19,13 @@ class Command(BaseModel):
     command = CharField()
 
 
-class Run(BaseModel)
+class Run(BaseModel):
     command = ForeignKeyField(Command, related_name='runs')
     run_time = IntegerField()
+    ran_at = DateTimeField()
 
 
 class Measure(BaseModel):
     run = ForeignKeyField(Run, related_name='measures')
     cpu = IntegerField()
     memory = IntegerField()
-
